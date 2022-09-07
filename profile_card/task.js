3a0ab0d63bd1e46462c7b1bba2721128f7ef8cba
TwinMangz <93635763+TwinMangz@users.noreply.github.com>
2022-09-07 13:01:42 +0530

    Folder with files for Profile viewer

---
+++ profile_card/task.js
@@ -0,0 +1,96 @@
+const users = [
+    {
+        id      :   1,
+        name    :   "naruto",
+        age     :   "25",
+        mobile  :   12345,
+        pic     :   "naruto.png",
+        address :   {
+                        locality    : "block-1",
+                        district    : "area1",
+                        pincode     : "123",
+                        state       : "hidden leaf"
+                    }
+    },{
+        id      :   2,
+        name    :   "hinata",
+        age     :   "22",
+        mobile  :   12345,
+        pic     :   "hinata.jpg",
+        address :   {
+                        locality    : "block-2",
+                        district    : "area1",
+                        pincode     : "123",
+                        state       : "hidden leaf"
+                    }
+    },{
+        id      :   3,
+        name    :   "jack sparrow",
+        age     :   "30",
+        mobile  :   12345,
+        pic     :   "jack sparrow.jpg",
+        address :   {
+                        locality    : "block-3",
+                        district    : "area3",
+                        pincode     : "1234",
+                        state       : "kentucky"
+                    }
+    },{
+        id      :   4,
+        name    :   "luffy",
+        age     :   "25",
+        mobile  :   12345,
+        pic     :   "luffy.jpg",
+        address :   {
+                        locality    : "block-4",
+                        district    : "area4",
+                        pincode     : "12342",
+                        state       : "shibuya"
+                    }
+    },{
+        id      :   5,
+        name    :   "nezuko",
+        age     :   "20",
+        mobile  :   12345,
+        pic     :   "nezuko.jpg",
+        address :   {
+                        locality    : "block2",
+                        district    : "area1",
+                        pincode     : "123",
+                        state       : "kyushu-okinawa"
+                    }
+    }
+];
+
+
+
+const leftPanel = document.getElementById('left-panel');
+const mainPanel = document.getElementById("main-panel");
+
+let content = "",counter= 0;
+
+
+for(let user of users){
+    content += `
+    <span type="button" src="user[`+counter+`]" onmouseover="displayDetails('`+counter+`')">
+    <img src="`+user.pic+`" alt="`+user.name+`" class="panel-img" />
+    </span><br>`
+    counter += 1;
+    
+}
+
+leftPanel.innerHTML = content;
+
+function displayDetails(index){
+    const user = users[index];
+    const mainContent = `<div class="pcard">
+    <img src="`+user.pic+`" alt="`+user.name+`" class="displayimage" style="border-radius:500px;" />
+
+        <h4>Name: `+user.name+`</h4>
+        <h4>Age: `+user.age+`</h4>
+        <h4>Mobile: `+user.mobile+`</h4>
+        <h4>Address: `+user.address.locality+`,<br> &nbsp &nbsp &nbsp `+user.address.district+` , `+user.address.pincode +` , `+user.address.state+` </h4>
+
+    </div>`;
+    mainPanel.innerHTML = mainContent;
+}
