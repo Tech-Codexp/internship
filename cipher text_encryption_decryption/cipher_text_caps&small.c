d9da8cd4633a89f7b4170ee72fac71b95573d69c
TwinMangz <93635763+TwinMangz@users.noreply.github.com>
2022-09-10 07:34:57 +0530

    This is the update of cipher Text Encryption Decryption, character except alphabet are allow to print including spaces

--- cipher text_encryption_decryption/cipher_text_caps&small.c
+++ cipher text_encryption_decryption/cipher_text_caps&small.c
@@ -1,83 +1,80 @@
-#include<stdio.h>
-int Encrypt(int,int);
-int Decrypt(int,int);
+#include <stdio.h>
+int Encrypt(int, int);
+int Decrypt(int, int);
 int main()
 {
-	int salt,len,select,i,j,code,num;
-	char text[26],letter;
+	int salt, len, select, i, j, code, num;
+	char text[26], letter;
 
 	printf("this program is to encode or decode cipher text\n\n");
 	printf("enter the text:\t");
 	gets(text);
-	
+
 	printf("enter the value of salt:\t");
-	scanf("%d",&salt);
-	
-	len=strlen(text);
-	printf("length of string is : %d\n\n",len);
-	
+	scanf("%d", &salt);
+
+	len = strlen(text);
+	printf("length of string is : %d\n\n", len);
+
 	printf("Choose the Operation you want to perform\n\tEnter 1 for Encryption\n\tEnter 2 for Decryption\n");
-	scanf("%d",&select);
-	if(select==1)
+	scanf("%d", &select);
+	if (select == 1)
 	{
-		
-		for(i=0;i<len;i++)
+
+		for (i = 0; i < len; i++)
 		{
-			letter=text[i];
+			letter = text[i];
 			num = letter;
-			if(num!=32 && num>90)
+			if (num != 32 && num > 90)
 			{
-				num-=32;  
-				code=Encrypt(num,salt);  		//call function
-				code+=32;
-				
-				printf("%c",code);
+				num -= 32;
+				code = Encrypt(num, salt); // call function
+				code += 32;
+
+				printf("%c", code);
 			}
-			else if(num==32)
+			else if (num == 32)
 			{
-				code=num;
-				printf("%c",code);
+				code = num;
+				printf("%c", code);
 			}
 			else
 			{
-				
-				code=Encrypt(num,salt);			//call function
-				printf("%c",code);
+
+				code = Encrypt(num, salt); // call function
+				printf("%c", code);
 			}
-//			
+			//
 		}
-		
 	}
-	else if(select==2)
+	else if (select == 2)
 	{
-			for(i=0;i<len;i++)
+		for (i = 0; i < len; i++)
 		{
-			letter=text[i];
+			letter = text[i];
 			num = letter;
-			
-			if(num>90)
+
+			if (num > 90)
 			{
-				num-=32;  
-				code=Decrypt(num,salt);  		//call function
-				code+=32;
-				
-				printf("%c",code);
+				num -= 32;
+				code = Decrypt(num, salt); // call function
+				code += 32;
+
+				printf("%c", code);
 			}
-			else if(num==32)
+			else if (num == 32)
 			{
-				code=num;
-				printf("%c",code);
+				code = num;
+				printf("%c", code);
 			}
 			else
 			{
-				
-				code=Decrypt(num,salt);			//call function
-				printf("%c",code);
+
+				code = Decrypt(num, salt); // call function
+				printf("%c", code);
 			}
-//			
+			//
 		}
-		
-		
 	}
 	else
 	{
@@ -85,35 +82,33 @@ int main()
 	}
 }
 
-int Encrypt(value,shift){    // function for encryption
+int Encrypt(value, shift)
+{ // function for encryption
 
-		value-=65;
-		value = (value+shift)%26;
-					
-		return value+=65;
-			
+	value -= 65;
+	value = (value + shift) % 26;
+
+	return value += 65;
 }
-int Decrypt(value,shift){		// function for decryption
-		int temp;
-		
-		value-=65;
-		temp = (shift%26)*(-1);
-		
-		value = value - (-temp);
-		 		if(value<0)
-					{
-						value = value + 26;
-						
-						return value+=65;
-						
-					}else{
-						
-						return value+=65;
-					}
-		
-		return value+=65;	
+int Decrypt(value, shift)
+{ // function for decryption
+	int temp;
 
+	value -= 65;
+	temp = (shift % 26) * (-1);
+
+	value = value - (-temp);
+	if (value < 0)
+	{
+		value = value + 26;
+
+		return value += 65;
+	}
+	else
+	{
+
+		return value += 65;
+	}
+
+	return value += 65;
 }
-
-
-
